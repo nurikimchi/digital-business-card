ef71c5f367f64b6dd57f4c22931d3173d12f0f3d
Nuri Kim <64626132+nurikimchi@users.noreply.github.com>
2022-04-30 17:49:16 -0700

    Added child folders and files via upload.

---
+++ Components/Interests.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Interests() {
+    return (
+        <div className = "interests">
+            <h2 className = "interests__header">Interests</h2>
+            <p className = "interests__body">Golf; bogey player. Programming; semi-beginner.</p>
+        </div>
+    )
+}
