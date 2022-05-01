3e14b7e50a248f158f27270b6d7ac8f3ea9686c2
Nuri Kim <64626132+nurikimchi@users.noreply.github.com>
2022-04-30 17:48:12 -0700

    Added folder via upload.

---
+++ digital-business-card/Components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Footer() {
+    return (
+        <div className = "footer">
+            <img className = "footer__icon" id = "footer__instagram-icon" src = "https://cdn-icons-png.flaticon.com/512/87/87390.png"></img>
+            <img className = "footer__icon" id = "footer__github-icon" src = "https://cdn-icons-png.flaticon.com/512/25/25231.png"></img>
+        </div>
+    )
+}
